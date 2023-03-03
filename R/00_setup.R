23c53383802589dd4df9b8ac11670e26b68b0c1a
Christopher Kenny <ctk44@cornell.edu>
2023-03-02 19:34:53 -0500

    init

---
+++ R/00_setup.R
@@ -0,0 +1,29 @@
+suppressMessages({
+  # general ----
+  library(tidyverse)
+  library(fs)
+  library(here)
+  library(cli)
+  #library(gt)
+  library(readxl)
+
+  # geospatial ----
+  library(sf)
+
+  # alarm project ----
+  #library(redist)
+  library(geomander)
+  library(censable)
+  #library(tinytiger)
+  library(ggredist)
+  #library(alarmdata)
+
+  # plotting ----
+  library(wacolors)
+  library(patchwork)
+  library(scales)
+})
+
+#walk(dir_ls(path = here('R/utils/'), glob = '*.R'), source)
+
+cli_alert_success('Packages loaded and utilities prepared.')
---
+++ R/01_prep_data.R
@@ -0,0 +1,2 @@
+shp_chi <- read_sf(here('data-raw/Boundaries - Ward Precincts (2012-2022).geojson'))
+mayor_2023 <- readxl::read_excel(here('data-raw/2023-02-28_mayor.xlsx'))
